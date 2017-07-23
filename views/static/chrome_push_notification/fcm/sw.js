cc653995fbaa2881b7804d2339c2fa82fd9077a6
dptole <danillo.paiva.toledo@gmail.com>
2017-07-23 17:45:19 -0300

    - Bugfix "This site has been updated in the background" and refactor.

--- views/static/chrome_push_notification/fcm/sw.js
+++ views/static/chrome_push_notification/fcm/sw.js
@@ -8,72 +8,81 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging()
 
-messaging.setBackgroundMessageHandler(payload => {
-  if(payload.data)
-    displayNotification(payload.data)
-})
-
-addEventListener('notificationclick', event => {
-  event.notification.close()
-  if(isValidAction(event.action))
-    runNotificationAction(parseJSON(event.action).parsed)
-  else if(isValidAction(event.notification.tag))
-    runNotificationAction(parseJSON(event.notification.tag).parsed)
-})
-
-function runNotificationAction(action) {
-  if(action.type === 'read-news')
-    clients.openWindow(action.url)
-  else if(action.type === 'next-news')
-    messaging.getToken().then(token =>
-      fetch('https://dptole.ngrok.io/chrome_push_notification/get_news?token=' + token)
-    )
-}
-
-function ensureURL(url) {
-  return /^https?:\/\//i.test(url) ? url : 'https://firebase.google.com/_static/images/firebase/touchicon-180.png'
-}
-
-function isValidAction(action) {
-  return parseJSON(action).success
-}
-
-function displayNotification(notification) {
-  if(notification.type === 'article') {
-    const article = parseJSON(notification.article)
-    if(!article.success) return false
-    registration.showNotification(
-      article.parsed.source,
-      {
-        body: [article.parsed.title, article.parsed.description].join(' | '),
-        icon: ensureURL(article.parsed.urlToImage),
-        tag: JSON.stringify({type: 'read-news', url: article.parsed.url}),
-        actions: [{
-          title: 'Read',
-          action: JSON.stringify({type: 'read-news', url: article.parsed.url}),
-            icon: 'https://cdn0.iconfinder.com/data/icons/solid-line-essential-ui-icon-set/512/essential_set_menu-32.png'
-        }, {
-          title: 'Next',
-          action: JSON.stringify({type: 'next-news'}),
-          icon: 'https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_arrow_forward_48px-32.png'
-        }]
+const libs = {
+  ensureHTTPSURL(url) {
+    return /^https:\/\//i.test(url) ? url : 'https://firebase.google.com/_static/images/firebase/touchicon-180.png'
+  },
+  isValidAction(action) {
+    return libs.parseJSON(action).success
+  },
+  parseJSON(json_string) {
+    try {
+      return {
+        success: true,
+        parsed: JSON.parse(json_string)
       }
-    )
-    return true
+    } catch(error) {
+      return {
+        error
+      }
+    }
+  },
+  displayNotification(notification) {
+    if(notification.type === 'article') {
+      const article = libs.parseJSON(notification.article)
+      if(article.success)
+        return registration.showNotification(
+          article.parsed.source,
+          {
+            body: [article.parsed.title, article.parsed.description].join(' | '),
+            icon: libs.ensureHTTPSURL(article.parsed.urlToImage),
+            tag: JSON.stringify({type: 'read-news', url: article.parsed.url}),
+            actions: [{
+              title: 'Read',
+              action: JSON.stringify({type: 'read-news', url: article.parsed.url}),
+              icon: 'https://cdn0.iconfinder.com/data/icons/solid-line-essential-ui-icon-set/512/essential_set_menu-32.png'
+            }, {
+              title: 'Next',
+              action: JSON.stringify({type: 'next-news'}),
+              icon: 'https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_arrow_forward_48px-32.png'
+            }]
+          }
+        )
+    }
+  },
+  handleNotification(payload) {
+    return payload.data
+      ? libs.displayNotification(payload.data)
+      : registration.showNotification()
+  },
+  runNotificationAction(action) {
+    if(action.type === 'read-news')
+      clients.openWindow(action.url)
+    else if(action.type === 'next-news')
+      messaging.getToken().then(token =>
+        fetch('https://dptole.ngrok.io/chrome_push_notification/get_news?token=' + token)
+      )
+  },
+  eventHandlers: {
+    push(event) {
+      event.waitUntil(
+        libs.handleNotification(event.data.json()).catch(error => {
+          console.log('Error handling the notification')
+          console.log(error)
+          console.log('Event')
+          console.log(event)
+        })
+      )
+    },
+    notificationClick(event) {
+      event.notification.close()
+      if(libs.isValidAction(event.action))
+        libs.runNotificationAction(libs.parseJSON(event.action).parsed)
+      else if(libs.isValidAction(event.notification.tag))
+        libs.runNotificationAction(libs.parseJSON(event.notification.tag).parsed)
+    }
   }
-
-  return false
 }
 
-function parseJSON(json_string) {
-  try {
-    return {
-      success: true,
-      parsed: JSON.parse(json_string)
-    }
-  } catch(error) {
-    return {
-      error
-    }
-  }
-}
+addEventListener('push', libs.eventHandlers.push)
+addEventListener('notificationclick', libs.eventHandlers.notificationClick)
